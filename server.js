ac7b161f3490be998a27f4e7e5daed1f22881a17
rohansinha01 <rohansinha01@gmail.com>
2023-12-28 22:18:06 -0500

    update route

--- server.js
+++ server.js
@@ -20,17 +20,31 @@ db.on("closed", () => console.log("mongo disconnected"))
 app.use(morgan("dev"))
 app.use(express.urlencoded({extended: true}))
 
-
+// Index
 app.get("/animals", async (req,res) => {
     let allAnimals = await Animal.find({})
 
     res.send(allAnimals)
+})
+
+// Update - PUT
+app.put("/animals/:id", async (req, res) => {
+    const id = req.params.id
+    const newAnimal = req.body
+    let updatedAnimal = await Animal.findByIdAndUpdate(id, newAnimal)
+    res.send(updatedAnimal)
 })
 //CREATE - POST
 app.post("/animals", async (req, res) => {
     let newAnimal = await Animal.create(req.body)
     res.send(newAnimal)
     })
+
+//Show route
+app.get ("/animals/:id", async(req, res) => {
+    let foundAnimal = await Animal.findById(req.params.id)
+    res.send(foundAnimal)
+})
 
 
 app.listen(PORT, () => {
